5416dcd021d3cbe3fc1c4f42a321f2e0a0513526
dariem <dariemsaxer@gmail.com>
2022-04-20 20:41:07 -0600

    added mastermind and constructor, this class represents a game

---
+++ app/mastermind.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.ArrayList;
+
+public class mastermind {
+    ArrayList<Point> Points;
+    Integer Attemps;
+
+    public mastermind(ArrayList<Point> points, Integer attemps) {
+        this.Points = points;
+        this.Attemps = attemps;
+    }
+}
