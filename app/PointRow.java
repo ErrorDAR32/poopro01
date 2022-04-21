60b152477c267b3ad03532fabf081dfbc4cd3879
dariem <dariemsaxer@gmail.com>
2022-04-20 20:52:04 -0600

    added pointrow

---
+++ app/PointRow.java
@@ -0,0 +1,9 @@
+package app;
+
+import java.util.ArrayList;
+
+/** this class represents the four spaces where colors go, and the four spaces for the hints **/
+public class PointRow {
+    ArrayList<Point> guess;
+    ArrayList<Point> hints;
+}
