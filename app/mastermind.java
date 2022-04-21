25d8cd31b7a65d12d727961b29b36a1aebb21432
dariem <dariemsaxer@gmail.com>
2022-04-20 21:11:48 -0600

    added preliminary guess check (mastermind.CheckColors)

--- app/mastermind.java
+++ app/mastermind.java
@@ -3,11 +3,37 @@ package app;
 import java.util.ArrayList;
 
 public class mastermind {
-    ArrayList<Point> Points;
-    Integer Attemps;
+    Integer CurrentRow;
+    ArrayList<PointRow> Rows;
+    ArrayList<Integer> Solution;
 
-    public mastermind(ArrayList<Point> points, Integer attemps) {
-        this.Points = points;
-        this.Attemps = attemps;
+    /** constructs a game state with default conditions **/
+    public mastermind(ArrayList<Integer> solution) {
+        this.CurrentRow = 0;
+        this.Rows = new ArrayList<>();
+        this.Solution = solution;
+
+        for (int i=0; i<10; i++) {
+            Rows.add(new PointRow());
+        }
+    }
+    /** checks colors on current row
+     * output array of integers encode:
+     * 0= no hint
+     * 1= red hint (good color)
+     * 2= white hint (good color, good position)*/
+    public ArrayList<Integer> CheckColors() {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        PointRow currentRow = this.Rows.get(this.CurrentRow);
+
+        for(int i=0; i<4; i++) {
+            if (currentRow.getGuess().get(i).getColor() == this.Solution.get(i)) {
+                result.add(2);
+            } else if (this.Solution.contains(currentRow.getGuess().get(i).getColor())) {
+                result.add(1);
+            } else result.add(0);
+        }
+        return result;
     }
 }
